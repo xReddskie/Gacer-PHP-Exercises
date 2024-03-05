7f353b78234eb23293df4a183410ec07acbdb1a1
redd <rrbg2002@gmail.com>
2024-03-05 15:34:17 +0800

    Added Date Time Exercise to Main Branch

---
+++ Date_Time/DateTime.php
@@ -0,0 +1,18 @@
+<?php
+
+class DateTimeDisplay {
+    private $dateTime;
+
+    public function __construct() {
+        $this->dateTime = new DateTime(); 
+    }
+
+    public function displayDateTime($format = 'Y-m-d H:i:s') {
+        return $this->dateTime->format($format);
+    }
+}
+
+$dateDisplay = new DateTimeDisplay();
+
+echo "Current Date and Time: " . $dateDisplay->displayDateTime() . "<br>";
+echo "Formatted Date and Time: " . $dateDisplay->displayDateTime('F j, Y h:i:s A');
