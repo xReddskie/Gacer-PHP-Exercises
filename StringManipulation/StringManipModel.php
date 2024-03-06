a4cdfae3f07b5ce6e2768201c2a83aafd5615a66
redd <rrbg2002@gmail.com>
2024-03-06 10:17:27 +0800

    Added String Manipulation v1.0.0 to main branch.

---
+++ StringManipulation/StringManipModel.php
@@ -0,0 +1,15 @@
+<?php
+
+class StringManipModel {
+    public function concatenate($str1, $str2) {
+        return $str1 . $str2;
+    }
+
+    public function substring($str, $start, $length) {
+        return substr($str, $start, $length);
+    }
+
+    public function replace($str, $search, $replace) {
+        return str_replace($search, $replace, $str);
+    }
+}
